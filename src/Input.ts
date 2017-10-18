71ac0484dbae3a03b8135c69f8c12cdd267fbb48
tomohisa-osada <agekuta@gmail.com>
2017-10-18 15:49:49 +0900

    change whitespace type

--- src/Input.ts
+++ src/Input.ts
@@ -8,7 +8,7 @@ export class Input {
    * @param source input source for parsers
    * @param whitespace regex that parsers should skip
    */
-  constructor(public source: string, public whitespace?: RegExp) {
+  constructor(public source: string, public whitespace: RegExp | undefined) {
   }
 
   copy(source: string): Input {
